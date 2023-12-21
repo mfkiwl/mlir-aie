cbaec1c0fc110ec7e798e568462193dd050d9bf2
Eddie Richter <eddie.richter@amd.com>
2023-12-21 09:11:00 -0700

    Cleaning up test 9

--- test/unit_tests/aie/09_simple_shim_dma/test.cpp
+++ test/unit_tests/aie/09_simple_shim_dma/test.cpp
@@ -8,9 +8,6 @@
 //
 //===----------------------------------------------------------------------===//
 
-// TODO: GET RID OF THIS
-#define AIR_PCIE
-
 #include <cassert>
 #include <cmath>
 #include <cstdio>
